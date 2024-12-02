96ccb8528546df7c46aa227e31e2944ec9bd3ebd
Dmytro <dimabwhite98@gmail.com>
2024-12-02 20:50:28 +0000

    Day 1 prettied

--- Day 1/solutionP1.js
+++ Day 1/solutionP1.js
@@ -1,16 +1,25 @@
 const initialData = require("./initialData").initialData;
 const left = [];
 const right = [];
-const example =
-  "3   4\n" + "4   3\n" + "2   5\n" + "1   3\n" + "3   9\n" + "3   3";
-initialData.split("\n").map((row) => row.split("   ").map((num, index) => index %2 === 0? left.push(+num): right.push(+num)));
+
+initialData
+  .split("\n")
+  .map((row) =>
+    row
+      .split("   ")
+      .map((num, index) =>
+        index % 2 === 0 ? left.push(+num) : right.push(+num),
+      ),
+  );
 left.sort((a, b) => a - b);
 right.sort((a, b) => a - b);
-const result = left.map((num, index) => Math.abs(num - right[index])).reduce((a, b) => a + b);
+const result = left
+  .map((num, index) => Math.abs(num - right[index]))
+  .reduce((a, b) => a + b);
 
-console.log(result)
+console.log(result);
 
-module.exports={
-    left,
-    right
-}
+module.exports = {
+  left,
+  right,
+};
--- Day 1/solutionP2.js
+++ Day 1/solutionP2.js
@@ -1,5 +1,6 @@
 const { left, right } = require("./solutionP1");
 
-
-const result = left.map(l => right.map(r => l === r ? 1 : 0).reduce((a, b) => a + b) * l).reduce((a, b) => a + b);
+const result = left
+  .map((l) => right.map((r) => (l === r ? 1 : 0)).reduce((a, b) => a + b) * l)
+  .reduce((a, b) => a + b);
 console.log(result);
